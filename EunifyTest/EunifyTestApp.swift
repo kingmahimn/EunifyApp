572351b4d5579cf2e5e8335d66a40fc4ee42069c
Mahimn <kingmahimn@gmail.com>
2023-10-01 11:42:51 -0400

    Initial Commit

---
+++ EunifyTest/EunifyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EunifyTestApp.swift
+//  EunifyTest
+//
+//  Created by Mahimn Patel on 2023-10-01.
+//
+
+import SwiftUI
+
+@main
+struct EunifyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
